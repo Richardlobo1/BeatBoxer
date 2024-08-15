53c57f7e7354e661e55d707956615feb1665d2f3
Richard lobo <112796052+Richardlobo1@users.noreply.github.com>
2024-08-15 17:45:57 +0530

    Add initial project files

---
+++ indexx.js
@@ -0,0 +1,3 @@
+var numberofDrumButtons = document.querySelectorAll(".drum").length;
+for (var i = 0; i < numberofDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
